e8a495d07b080b14ee4243904b58d1addf62d473
Francis Tsang <frtsang40@gmail.com>
2020-03-31 10:35:07 -0700

    added search form

--- app.js
+++ app.js
@@ -12,10 +12,9 @@ app.set('view engine', 'handlebars');
 
 // Routes
 app.get('/', (req, res) => {
-    // sets url of gif
-    const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
-    // renders gif view
-    res.render('hello-gif', { gifUrl });
+    console.log(req.query) // => "{ term: hey" }
+
+    res.render('home')
 });
 
 
